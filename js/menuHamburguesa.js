56f4f3d9bf10bf63c139e6304593cfb6f5b75484
Gustavo Baranda <baranda.gustavo@gmail.com>
2025-04-02 18:11:40 -0300

    fix menu hamburguesa

--- js/menuHamburguesa.js
+++ js/menuHamburguesa.js
@@ -1,20 +1,25 @@
 export function toggleMenu() {
-  document.getElementById("hamburger").addEventListener("click", (e) => {
-    e.stopPropagation();
-    const menu = document.getElementById("menu");
-    const hamburger = document.getElementById("hamburger");
+  const menu = document.getElementById("menu");
+  const hamburger = document.getElementById("hamburger");
+  const navLinks = document.querySelectorAll("#menu a");
 
+  hamburger.addEventListener("click", (e) => {
+    e.stopPropagation();
     menu.classList.toggle("active");
     hamburger.classList.toggle("active");
   });
 
   document.addEventListener("click", (e) => {
-    const menu = document.getElementById("menu");
-    const hamburger = document.getElementById("hamburger");
-
     if (!menu.contains(e.target) && !hamburger.contains(e.target)) {
       menu.classList.remove("active");
       hamburger.classList.remove("active");
     }
+  });
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      menu.classList.remove("active");
+      hamburger.classList.remove("active");
+    });
   });
 }
